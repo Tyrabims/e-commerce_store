632c9dc43df85df51cd448f6fc270d7bde352563
Tyrabims <abimbola.anibaba@yahoo.com>
2023-09-08 23:16:43 +0100

    Hero build in progress

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <h2>We are Celestial,</h2>
+    <h5>we design and manufacture furniture fit for the gods</h5>
+    <button>Discover Our Collections</button>
+    </>
+  )
+}
+
+export default Hero
